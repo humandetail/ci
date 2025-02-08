5dbfedbd899f3e1d3912b3ca6fdbd2393fbfcb17
laihanbin <512412344@qq.com>
2025-02-08 16:59:37 +0800

    chore: ci

--- packages/entry/src/config/games.config.ts
+++ packages/entry/src/config/games.config.ts
@@ -6,37 +6,37 @@ export interface GameConfig {
 
 export const games: GameConfig[] = [
   {
-    path: '/mini-games/games/mine-sweeper/index.html',
+    path: '/ci/mine-sweeper/index.html',
     name: '扫雷',
     cover: 'mine-sweeper.svg'
   },
   {
-    path: '/mini-games/games/link-game/index.html',
+    path: '/ci/link-game/index.html',
     name: '连连看',
     cover: 'link-game.svg'
   },
   {
-    path: '/mini-games/games/greedy-snake/index.html',
+    path: '/ci/greedy-snake/index.html',
     name: '贪吃蛇',
     cover: 'greedy-snake.svg'
   },
   {
-    path: '/mini-games/games/tetris/index.html',
+    path: '/ci/tetris/index.html',
     name: '俄罗斯方块',
     cover: 'tetris.svg'
   },
   {
-    path: '/mini-games/games/2048/index.html',
+    path: '/ci/2048/index.html',
     name: '2048',
     cover: '2048.svg'
   },
   {
-    path: '/mini-games/games/spider-solitaire/index.html',
+    path: '/ci/spider-solitaire/index.html',
     name: '蜘蛛纸牌',
     cover: 'spider-solitaire.svg'
   },
   {
-    path: '/mini-games/games/chinese-chess/index.html',
+    path: '/ci/chinese-chess/index.html',
     name: '中国象棋',
     cover: 'Chinese-chess.jpg'
   },
